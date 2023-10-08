0f6651e1e4dd27dc4b4ccd43f156eec638e5de97
benjamin-nartey <nartbillionz@gmail.com>
2023-10-08 14:36:04 +0000

    feat: add HeroCarousel, Navbar, PriceInfoCard , ProductCard, Searchbar, ProductModels, ProductDetails, ConnectToDB, to project

---
+++ lib/actions/index.ts
@@ -0,0 +1,92 @@
+"use server";
+import { revalidatePath } from "next/cache";
+import { connectToDB } from "../mongoose";
+import { scrapeAmazonProduct } from "../scraper";
+import Product from "../models/product.models";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
+
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if (!productUrl) return;
+
+  try {
+    connectToDB();
+
+    const scrapedProduct = await scrapeAmazonProduct(productUrl);
+
+    if (!scrapedProduct) return;
+
+    let product = scrapedProduct;
+
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
+
+    if (existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice },
+      ];
+
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        heighestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      };
+    }
+
+    const newProduct = await Product.findOneAndUpdate(
+      {
+        url: scrapedProduct.url,
+      },
+      product,
+      { upsert: true, new: true }
+    );
+    revalidatePath(`/products/${newProduct._id}`);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`);
+  }
+}
+
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
+
+    const product = await Product.findOne({ _id: productId });
+
+    if (!product) return null;
+
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getAllProduct() {
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if (!currentProduct) return null;
+
+    const similarProduct = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProduct;
+  } catch (error) {
+    console.log(error);
+  }
+}
