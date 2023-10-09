86edc252d6247ca76594dafe1bea2def85ad98a7
benjamin-nartey <nartbillionz@gmail.com>
2023-10-09 19:06:15 +0000

    added cron job to scrape and fire email automatically

--- lib/models/product.models.ts
+++ lib/models/product.models.ts
@@ -15,7 +15,7 @@ const productSchema = new mongoose.Schema(
       },
     ],
     lowestPrice: { type: Number },
-    heighestPrice: { type: Number },
+    highestPrice: { type: Number },
     averagePrice: { type: Number },
     discountRate: { type: Number },
     description: { type: String },
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -70,17 +70,17 @@ export async function scrapeAmazonProduct(url: string) {
       title,
       currentPrice: Number(currentPrice) || Number(originalPrice),
       originalPrice: Number(originalPrice) || Number(currentPrice),
+      priceHistory:[],
       discountRate: Number(discountRate),
       category: "category",
       reviewsCount: 100,
       stars: 4.5,
       isOutOfStock: outOfStock,
       description,
-      lowestPrice:Number(currentPrice) || Number(originalPrice),
-      heighestPrice: Number(originalPrice) || Number(currentPrice),
-      averagePrice:Number(currentPrice) || Number(originalPrice),
-
-    }
+      lowestPrice: Number(currentPrice) || Number(originalPrice),
+      highestPrice: Number(originalPrice) || Number(currentPrice),
+      averagePrice: Number(currentPrice) || Number(originalPrice),
+    };
     // console.log(data)
 
     return data;
